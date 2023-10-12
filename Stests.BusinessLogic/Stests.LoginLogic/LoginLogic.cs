85186241efec66cc7b595c71541606fae6f233e3
lveeeeeeeeeee <94752867+lveeeeeeeeeee@users.noreply.github.com>
2023-10-12 09:20:32 +0500

    Updated login logic

--- Stests.BusinessLogic/Stests.LoginLogic/LoginLogic.cs
+++ Stests.BusinessLogic/Stests.LoginLogic/LoginLogic.cs
@@ -1,7 +1,10 @@
 ﻿namespace Stests.LoginLogic
 {
+    using Stests.BusinessLogic.Models;
     using Stests.Database;
     using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
 
     public static class LoginLogic
     {
@@ -42,11 +45,47 @@
                 .Select(x => x)
                 .Where(x => x.Login == login);
 
-            if (users.Count() == 0)
+            if (users == null)
             {
                 return false;
             }
             return true;
+        }
+
+        /// <summary>
+        /// Check if login and password pair is valid.
+        /// </summary>
+        /// <param name="login">Login string.</param>
+        /// <param name="password">Password hash string.</param>
+        /// <returns></returns>
+        public static bool TryLogin(string login, string password)
+        {
+            // Convert password to byte array. 
+            byte[] passwordByteArray = password.ToCharArray()
+            .Select(x => (byte)x)
+            .ToArray();
+            // Encode the password with SHA-256 algorythm.
+            string passwordHash = "";
+            using (SHA256 cypherSHA256 = SHA256.Create())
+            {
+                byte[] hashValue = cypherSHA256.ComputeHash(passwordByteArray);
+                passwordHash = Encoding.Default.GetString(hashValue);
+            }
+
+            // Check if login and encoded password pair is present in the database.
+            using (AppDbContext context = new DefaultDbContextFactory().CreateDbContext())
+            {
+                var userInfo = context
+                    .UserInfo
+                    .Select(x => x)
+                    .Where(x => x.Login == login && x.PasswordHash == passwordHash);
+
+                if (userInfo == null)
+                {
+                    return false;
+                }
+                return true;
+            }
         }
     }
 }
